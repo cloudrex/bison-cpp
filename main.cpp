a0bf7e7e61dde507bdd39ebfdacaa2ae3172d378
CloudRex <cloudrex@outlook.com>
2019-01-02 17:40:51 -0500

    Use initial makefile

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream> // For 'cout'
+#include "llvm/IR/Module.h"
 
 int yyparse (void);
 
 using namespace std;
+using namespace llvm;
 
 int main() {
     printf("--- Program start ---\n");
--- old.makefile
+++
@@ -1,6 +0,0 @@
-default:
-	rm -rf build
-	mkdir -p build
-	bison -d grammar.y -o build/grammar.cc
-	flex -o build/lex.cc --header-file="build/lex.h" lex.l
-	g++ -o build/app build/lex.cc build/grammar.cc main.cpp
